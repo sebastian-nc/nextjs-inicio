856b4a3e0cb17683257bcb78b2039d0647c270e7
sebastian <scsebastian1406@gmail.com>
2026-01-14 12:32:26 -0500

    agregar host de confianza

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'placehold.co',
+        protocol: 'https'
+      },
+      {
+        hostname: 'picsum.photos',
+        protocol: 'https'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
